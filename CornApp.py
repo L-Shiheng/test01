206ff12b09bf3f690ec885f66abaaecf75e559ae
L-Shiheng <spklsh@shimadzu.com.cn>
2026-05-26 10:41:59 +0800

    Update CornApp.py

--- CornApp.py
+++ CornApp.py
@@ -13,9 +13,9 @@ warnings.filterwarnings('ignore')
 
 # ========== 1. 路径与设备配置 ==========
 # 如果部署到 GitHub，建议改为相对路径，例如：IMPUTER_SCALER_PATH = 'corn_treat.pkl'
-IMPUTER_SCALER_PATH = 'D:\\AIOnline\\corn\\corn_treat.pkl'   
-LNN_MODEL_PATH = 'D:\\AIOnline\\corn\\LNNclassification.pt'     
-LOG_FILE_PATH = 'D:\\AIOnline\\corn\\user_agreement_log.txt'
+IMPUTER_SCALER_PATH = './corn_treat.pkl'   
+LNN_MODEL_PATH = './LNNclassification.pt'     
+LOG_FILE_PATH = './user_agreement_log.txt'
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
 # ==========================================
